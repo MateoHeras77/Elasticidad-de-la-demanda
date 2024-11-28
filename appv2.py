e60facbfbad01ef93691b9f19dbf4c0e4e25c182
Mateo Heras <wmateohv@gmail.com>
2024-11-28 15:47:29 -0500

    Remove unnecessary whitespace and add a comment placeholder in appv2.py

--- appv2.py
+++ appv2.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 from plotly.subplots import make_subplots
-
+#hola
 def calculate_elasticities_and_prices(df):
     models_by_productline = {}
     elasticities_by_productline = {}
